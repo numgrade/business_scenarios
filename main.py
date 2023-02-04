404f6e83cae186fe6d7d18388a35cb445be5ef12
numgrade <num@numgrade.com>
2023-02-04 04:06:30 +0100

    feat: add columns for number of days

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 """Business scenarios with streamlit."""
 
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import streamlit as st
 
-from style import style_negative
+from business_model.style import style_negative
 
 
 def income(
@@ -42,11 +45,14 @@ def model(
             + month
             * (spending + income(ndays_subcontract, ndays_direct, ndays_outsourced))
         )
-    df = pd.DataFrame({"fund": fund}, index=range(months))
+    df = pd.DataFrame({"fund [euros]": fund}, index=range(months))
     # add a column variation per month
-    df["variation"] = df.diff().fillna(0)
+    df["revenue"] = df.diff().fillna(0)
+    df["ndays subcontract"] = ndays_subcontract
+    df["ndays direct"] = ndays_direct
+    df["ndays outsourced"] = ndays_outsourced
     # naming index
-    df.index.name = 'month'
+    df.index.name = "month"
     return df, df.iloc[-1][0]
 
 
@@ -70,7 +76,7 @@ if __name__ == "__main__":
     period3, fund = model(
         npersons=3,
         ndays_subcontract=8.5,
-        ndays_direct=8.6,
+        ndays_direct=4.6,
         ndays_outsourced=0,
         starting_fund=fund,
         months=7,
@@ -81,8 +87,55 @@ if __name__ == "__main__":
     result.index.name = period1.index.name
     print(result)
     # apply red color to negative values - for display in notebook
-    result = result.style.applymap(style_negative, props='color:red;')
+    result_redcolor = result.style.applymap(style_negative, props="color:red;")
 
-    # Streamlit app
-    st.title('Business model')
-    st.write(result)
+    # # Streamlit app
+    # st.title('Business model')
+    # st.write(result_redcolor)
+    # st.bar_chart(result["fund"])
+    # st.line_chart(result["revenue"])
+
+    # plotly
+    # result.reset_index(inplace=True)
+    # fig = px.line(result, x="month", y="revenue", color=px.Constant("Revenue"))
+    # fig.add_bar(result, x="month", y="fund [euros]", name="Fund")
+    # fig = go.Figure()
+    # Create figure with secondary y-axis
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.add_trace(
+        go.Scatter(
+            x=result.index,
+            y=result["revenue"],
+            marker=dict(color="SteelBlue"),
+            name="Revenue",
+            # secondary_y=False,
+        )
+    )
+    fig.add_trace(
+        go.Bar(
+            x=result.index,
+            y=result["fund [euros]"],
+            marker=dict(color="LightSalmon"),
+            name="Fund",
+            # secondary_y=False,
+        )
+    )
+    fig.add_trace(
+        go.Bar(
+            x=result.index,
+            y=result[["ndays subcontract", "ndays direct", "ndays outsourced"]],
+            # marker=dict(color='LightSalmon'),
+            # name='Fund',
+            # secondary_y=True,
+        )
+    )
+
+    # Set x-axis title
+    fig.update_xaxes(title_text="months")
+
+    # Set y-axes titles
+    fig.update_yaxes(title_text="<b>fund</b> [euros]", secondary_y=False)
+    fig.update_yaxes(title_text="<b># days</b>", secondary_y=True)
+
+    fig.show()
